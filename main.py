7fa09e74aab6253d9cd65aae711eb73c7531786f
hans.gillis <Vergeten1!>
2016-01-05 19:25:52 +0100

    cleaning up folders

---
+++ main.py
@@ -0,0 +1,18 @@
+from examples.tutorial.class_ball import main as class_ball_main
+
+def main():
+    """
+    To run this example on windows:
+
+    1) - open kivy-bash-2.7.bat
+       - change directory to root folder
+       - python main.py
+
+    2) - open command prompt
+       - change directory to root folder
+       - ..\kivy-2.7.bat main.py
+    """
+    class_ball_main()
+
+if __name__ == "__main__":
+    main()
